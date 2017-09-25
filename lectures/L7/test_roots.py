a29fc7eb45e428a19a7e7b29ccb83918a0708396
Jiawen Tong <tongjw1@outlook.com>
2017-09-25 15:05:06 -0400

    modified L7 Exercise1

--- lectures/L7/test_roots.py
+++ lectures/L7/test_roots.py
@@ -14,3 +14,18 @@ def test_quadroots_zerocoeff():
         roots.quad_roots(a=0.0)
     except ValueError as err:
         assert(type(err) == ValueError)
+
+def test_linearoots_result():
+    assert roots.linear_roots(2.0, -3.0) == 1.5
+
+def test_linearroots_types():
+    try:
+        roots.linear_roots("ocean", 6.0)
+    except TypeError as err:
+        assert(type(err) == TypeError)
+
+def test_linearroots_zerocoeff():
+    try:
+        roots.linear_roots(a=0.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
